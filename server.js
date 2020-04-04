50f4ec9cb1e7465716b5d91ef24cb59637c388de
hussein <hussein66253@icloud.com>
2020-04-04 17:17:32 +0300

    test3

--- server.js
+++ server.js
@@ -1,36 +1,50 @@
+// 'use strict'
+
+
+// const express = require('express');
+
+// const server = express();
+
+// const Port = process.env.Port || 3000;
+
+// server.use(express.static('./public'));
+
+
+// http://localhost:3000/test
+// server.get('/test', (request, response) => {
+//   response.send('zzzzzzzzzzzzzzzzz');
+
+// });
+// server.use('*', (request,response) =>{
+//   response.send('sorry this page not fond');
+// })
+// // http://localhost:3000/data
+
+// // server.get('/data',(request, response) =>{
+// //  let zzzzzzz = [
+// //      {name:"zzzzzz"}, 
+// //      {name:"zzzzzz"},
+// //      {name:"zzzzzz"}
+// //     ];
+// //     response.json(zzzzzzz); 
+// // });
+
+
+// server.listen(Port, () => {
+//   console.log('listening on port', Port);
+
+// });
 'use strict'
-
-
 const express = require('express');
-
 const server = express();
-
-const Port = process.env.Port || 3000;
-
+const PORT = process.env.PORT || 3000;
 server.use(express.static('./public'));
-
-
-http://localhost:3000/test
-server.get('/test', (request, response) => {
-  response.send('zzzzzzzzzzzzzzzzz');
-
+server.get('/', (request, response) => {
+    response.send('You are doing great');
 });
 server.use('*', (request,response) =>{
-  response.send('sorry this page not fond');
+    response.send('sorry this page not fond');
 })
-// http://localhost:3000/data
-
-// server.get('/data',(request, response) =>{
-//  let zzzzzzz = [
-//      {name:"zzzzzz"}, 
-//      {name:"zzzzzz"},
-//      {name:"zzzzzz"}
-//     ];
-//     response.json(zzzzzzz); 
-// });
-
-
-server.listen(Port, () => {
-  console.log('listening on port', Port);
-
+server.listen(PORT, () => {
+    console.log('hello there that is my port ', PORT);
 });
